0d24f09f48f6587ca82d01f5beb628271f7405c0
Ahmed-Mohmed-Sakr <ahmedsakr07407@gmail.com>
2023-10-18 14:41:28 +0300

    implement guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,11 @@ import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { SignupComponent } from './pages/signup/signup.component';
 import { WellcomComponent } from './pages/wellcom/wellcom.component';
+import { authGuard } from './guards/auth.guard';
 
 const routes: Routes = [
   { path: '', component: WellcomComponent },
-  { path: 'home', component: HomeComponent },
+  { path: 'home', component: HomeComponent, canActivate: [authGuard] },
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
 ];
--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,5 +1,13 @@
-import { CanActivateFn } from '@angular/router';
+import { CanActivateFn, Router } from '@angular/router';
+import { getToken } from '../environments/environments';
+import { inject } from '@angular/core';
 
 export const authGuard: CanActivateFn = (route, state) => {
-  return true;
+  if (getToken()) {
+    return true;
+  } else {
+    const router = inject(Router);
+
+    return router.navigate(['/'])
+  }
 };
